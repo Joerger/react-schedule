b39b64a4db67cab3573ce359172f4b3d1f69c6d7
cazinge <eddie.azinge@gmail.com>
2017-03-19 13:53:44 -0700

    structured demo site

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Schedule from './Schedule';
+
+function Home() {
+  return (
+    <div className="container">
+      <br />
+      <div className="row center">
+        <Schedule />
+      </div>
+      <br />
+    </div>
+  );
+}
+
+export default Home;
